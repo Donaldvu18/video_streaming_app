8cd66a2ea189623c3ac86189c906f9e006eb55eb
Don Vu <dvu37@gatech.edu>
2019-12-18 15:11:44 -0800

    make Redux fomr into its own component called StreamForm and refactored other components to use it

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,7 +12,7 @@ class StreamList extends React.Component{
     renderAdmin(stream){
         if (stream.userId=== this.props.currentUserId){
             return <div className='right floated contet'>
-                <button className='ui button primary'>Edit</button>
+               <Link to={`/streams/edit/${stream.id}`} className='ui button primary'>Edit</Link>
                 <button className='ui button negative'>Delete</button>
                 </div>
         }
