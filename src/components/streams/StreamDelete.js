44ce9f1386dc83ed4f08698608a8af3068eb7bad
Don Vu <dvu37@gatech.edu>
2019-12-18 18:26:20 -0800

    change modal delete caption to be dynamic

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamDelete = () => {
-    const actions= (
+class StreamDelete extends React.Component{
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    renderActions(){return(
         <React.Fragment>
             <button className='ui button negative'>Delete</button>
             <button className='ui button'>Cancel</button>
         </React.Fragment>
     )
-    return (
-        <div>StreamDelete<Modal onDismiss={()=>history.push('/')} title='Delete Stream' content='Are you sure you want to deleet this stream?' actions={actions}/></div>
-    );
+    }
+    renderContent(){
+        if(!this.props.stream){
+            return 'Are you sure you want to delete this stream?'
+        }
+        return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`
+    }
+    render(){
+        return (
+            <Modal onDismiss={()=>history.push('/')} title='Delete Stream' content={this.renderContent()} actions={this.renderActions()}/>
+        );
+}
+}
+
+const mapStateToProps=(state,ownProps)=>{
+    return{stream:state.streams[ownProps.match.params.id]}
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps,{fetchStream})(StreamDelete);
